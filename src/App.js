f545f43c30b8ac0300cec1b7e0384492ef8aa2b1
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-12-28 22:44:49 +0000

    improved search functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useImperativeHandle } from 'react'
 import axios from 'axios'
 
 export default function App() {
   const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('reacthooks')
+  const [query, setQuery] = useState('react hooks')
 
   useEffect(() => {
     getResults()
-  }, [query]);
+  }, []);
 
   const getResults = async ()  => {
     const response = await axios.get(
@@ -16,9 +16,21 @@ export default function App() {
   setResults(response.data.hits);
   }
 
+  const handleSearch = event => {
+    event.preventDefault()
+    getResults()
+  }
+
   return(
     <>
-    <input type='text' onChange={event => setQuery(event.target.value)} />
+    <form onSubmit={handleSearch}>
+      <input
+        type='text'
+        onChange={event => setQuery(event.target.value)}
+        value={query}
+        />
+      <button type='submit'>Search Articles</button>
+    </form>
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
