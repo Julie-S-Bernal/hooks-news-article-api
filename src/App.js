db3fce508c8261859e74d318092110b830662b4b
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-12-28 22:58:52 +0000

    fixes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import axios from 'axios'
 
 export default function App() {
   const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('react hooks')
-  const [loading, setLoading] = useState(false)
-  const searchInputRef = useRef()
+  const [query, setQuery] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
 
   useEffect(() => {
     getResults()
@@ -13,11 +14,15 @@ export default function App() {
 
   const getResults = async ()  => {
     setLoading(true)
-    const response = await axios.get(
-      `http://hn.algolia.com/api/v1/search?query=${query}`
-    );
-  setResults(response.data.hits);
-  setLoading(false)
+    try{
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setResults(response.data.hits);
+    } catch(error) {
+      setError(error)
+    }
+    setLoading(false)
   }
 
   const handleSearch = event => {
@@ -31,27 +36,28 @@ export default function App() {
 
   return(
     <>
-    <form onSubmit={handleSearch}>
-      <input
-        type='text'
-        onChange={event => setQuery(event.target.value)}
-        value={query}
-        ref={searchInputRef}
-        />
-      <button type='submit'>Search Articles</button>
-      <button onClick={handleClearSearch} type='button'>Clear search</button>
-    </form>
+      <form onSubmit={handleSearch}>
+        <input
+          type='text'
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+          ref={searchInputRef}
+          />
+        <button type='submit'>Search Articles</button>
+        <button onClick={handleClearSearch} type='button'>Clear search</button>
+      </form>
       {loading ? (
-      <div>Loading articles ...</div>
-       ) : (
-      <ul>
-        {results.map(result => (
-          <li key={result.objectID}>
-            <a href={result.url}>{result.title}</a>
-          </li>
-        ))}
-      </ul>
+        <div>Loading articles ...</div>
+      ) : (
+        <ul>
+          {results.map(result => (
+            <li key={result.objectID}>
+              <a href={result.url}>{result.title}</a>
+            </li>
+          ))}
+        </ul>
       )}
+      { error && <div>{error.message}</div>}
     </>
   )
 }
