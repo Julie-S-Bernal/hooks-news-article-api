578ffb389897374e666d840770c51d829fa80fee
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-12-28 22:54:05 +0000

    added a loading state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useState, useImperativeHandle } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
 
 export default function App() {
   const [results, setResults] = useState([]);
   const [query, setQuery] = useState('react hooks')
+  const [loading, setLoading] = useState(false)
+  const searchInputRef = useRef()
 
   useEffect(() => {
     getResults()
   }, []);
 
   const getResults = async ()  => {
+    setLoading(true)
     const response = await axios.get(
       `http://hn.algolia.com/api/v1/search?query=${query}`
     );
   setResults(response.data.hits);
+  setLoading(false)
   }
 
   const handleSearch = event => {
     event.preventDefault()
     getResults()
+  }
+  const handleClearSearch = () => {
+    setQuery('');
+    searchInputRef.current.focus()
   }
 
   return(
@@ -28,9 +36,14 @@ export default function App() {
         type='text'
         onChange={event => setQuery(event.target.value)}
         value={query}
+        ref={searchInputRef}
         />
       <button type='submit'>Search Articles</button>
+      <button onClick={handleClearSearch} type='button'>Clear search</button>
     </form>
+      {loading ? (
+      <div>Loading articles ...</div>
+       ) : (
       <ul>
         {results.map(result => (
           <li key={result.objectID}>
@@ -38,6 +51,7 @@ export default function App() {
           </li>
         ))}
       </ul>
+      )}
     </>
   )
 }
